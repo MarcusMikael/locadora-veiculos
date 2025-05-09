a6aa4b17b400c13e06faebaa767d94f7b6ab6d8b
Marcus Mikael <marcusmikael2@gmail.com>
2025-05-09 12:52:40 -0300

    Update consulta.sql

--- dml/consulta.sql
+++ dml/consulta.sql
@@ -6,7 +6,9 @@ SELECT * FROM pagamento;
 SELECT * FROM manutencao;
 
 -- Veiculos com ano de fabricação maior que 2019.
-SELECT * FROM veiculos WHERE ano > 2019;
+SELECT v.modelo, v.ano
+FROM veiculos v 
+WHERE ano > 2019;
 
 -- Clientes ordenados pelo nome.
 SELECT * FROM clientes ORDER BY nome;
@@ -14,7 +16,7 @@ SELECT * FROM clientes ORDER BY nome;
 -- Cliente que começa com o nome Marcus.
 SELECT * FROM clientes WHERE nome LIKE 'Marcus%';
 
--- Veiculos com status disponíveis.
+-- Veiculos do tipo moto com status disponíveis.
 SELECT * FROM veiculos WHERE status = 'Disponível' AND tipo = 'Moto';
 
 -- Informação geral do locamento, com nome do cliente, e informações do pagamento.
@@ -25,15 +27,20 @@ JOIN veiculos v ON l.fk_id_veiculo = v.id_veiculo
 JOIN pagamento p ON p.fk_id_locacao = l.id_locacao;
 
 -- Mostra locações com valor acima de 300R$.
-SELECT * FROM locacao WHERE valor_total_loc > 300 ORDER BY valor_total_loc DESC;
+SELECT c.nome AS nome_cliente, l.data_loc AS data_locacao, l.valor_total_loc AS valor_locacao
+FROM locacao l
+JOIN clientes c ON l.fk_id_cliente = c.id_cliente
+WHERE l.valor_total_loc > 300 ORDER BY valor_total_loc DESC;
 
 -- Mostrar locações com data inicio de 12/03 a 30/03 de 2025.
-SELECT * FROM locacao WHERE data_loc BETWEEN '2025-03-12' AND '2025-03-30';
+SELECT l.data_loc AS data_locacao, l.data_fim_loc AS data_final, l.valor_total_loc AS valor_locacao
+FROM locacao l
+WHERE data_loc BETWEEN '2025-03-12' AND '2025-03-30';
 
 -- Mostrar quantidade de locação, feito por determinado cliente.
 SELECT c.nome, COUNT(l.id_locacao) AS quantidade_locacao
 FROM clientes c
-JOIN locacao l ON l.fk_id_cliente = c.id_cliente
+LEFT JOIN locacao l ON l.fk_id_cliente = c.id_cliente
 GROUP BY c.nome;
 
 -- Mostrar manutenção feita, e gasto de determinado veiculo.
@@ -43,4 +50,6 @@ JOIN veiculos v ON m.fk_id_veiculo = v.id_veiculo
 GROUP BY fk_id_veiculo, v.modelo, m.descricao;
 
 -- Mostrar pagamentos feito por PIX e Cartão.
-SELECT * FROM pagamento WHERE tipopag IN ('PIX', 'Cartão');
+SELECT p.tipo_pag, p.data_pag, p.valor_pag
+FROM pagamento p
+WHERE tipo_pag IN ('PIX', 'Cartão');
